1aff7ebbc3a3851e67268799f79653ba1a9491a7
dev-sm2 <saimain.dev@gmail.com>
2020-04-09 14:38:21 +0630

    Gallaries Test

--- app/Http/Resources/OverviewResource.php
+++ app/Http/Resources/OverviewResource.php
@@ -21,9 +21,6 @@ class OverviewResource extends JsonResource
 
             ],
             [
-                'text' => 'စောင့်ကြည့်လူနာ (PUI) - ' . $this['pui'] . ' ဦး ၊ ' . 'သံသယ လူနာ (Suspected) - ' . $this['suspected'] . ' ဦး ၊ ' . 'ပိုးတွေ့ လူနာ (Lab Confirmed) - ' . $this['confirm'] . ' ဦး ၊ ' . 'ပိုးမတွေ့ လူနာ (Lab Negative) - ' . $this['lab_negative'] . ' ဦး ၊ ' . 'ဓာတ်ခွဲအဖြေစောင့်ဆိုင်းဆဲ လူနာ (Lab Pending) - ' . $this['lab_pending'] . ' ဦး ၊ ' . 'ပိုးတွေ့သေဆုံး လူနာ (Death among Lab Confirmed) - ' . $this['deaths'] . ' ဦး ၊ ' . 'ပိုးတွေ့ပြန်လည်သက်သာ လူနာ (Recovered) - ' . $this['recovered'] . ' ဦး ရှိပါသည်။',
-            ],
-            [
                 'attachment' => [
                     'type' => 'template',
                     'payload' => [
@@ -31,8 +28,8 @@ class OverviewResource extends JsonResource
                         'image_aspect_ratio' => 'horizontal',
                         'elements' => [
                             [
-                                'title' => $this['pui'] . ' ဦး',
-                                'image_url' => 'https://i.ibb.co/WKgn41x/PUI-1.png',
+                                'title' => 'စောင့်ကြည့် လူနာ ' . $this['pui'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/JxPQxNr/one.png',
                                 'subtitle' => $this['dt'],
                                 'buttons' => [[
 
@@ -42,8 +39,8 @@ class OverviewResource extends JsonResource
                                 ]]
                             ],
                             [
-                                'title' => $this['suspected'] . ' ဦး',
-                                'image_url' => 'https://i.ibb.co/rkMd3Zx/s.png',
+                                'title' => 'သံသယ လူနာ ' .  $this['suspected'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/ykgkNTQ/two.png',
                                 'subtitle' => $this['dt'],
                                 'buttons' => [[
 
@@ -53,8 +50,51 @@ class OverviewResource extends JsonResource
                                 ]]
                             ],
                             [
-                                'title' => $this['confirm'] . ' ဦး',
-                                'image_url' => 'https://i.ibb.co/7CRTFPH/sa.png',
+                                'title' => 'ပိုးတွေ့ လူနာ ' .  $this['confirm'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/3yY3tk6/three.png',
+                                'subtitle' => $this['dt'],
+                                'buttons' => [[
+
+                                    'type' => 'show_block',
+                                    'block_names' => ['Menu'],
+                                    'title' => 'မာတိကာ'
+                                ]]
+                            ],
+                            [
+                                'title' => 'ပိုးမတွေ့ လူနာ ' .  $this['lab_negative'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/vV9HMc1/four.png',
+                                'subtitle' => $this['dt'],
+                                'buttons' => [[
+
+                                    'type' => 'show_block',
+                                    'block_names' => ['Menu'],
+                                    'title' => 'မာတိကာ'
+                                ]]
+                            ],
+                            [
+                                'title' => 'ဓာတ်ခွဲအဖြေစောင့်ဆိုင်းဆဲ လူနာ ' .  $this['lab_pending'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/yh8GqcV/five.png',
+                                'subtitle' => $this['dt'],
+                                'buttons' => [[
+
+                                    'type' => 'show_block',
+                                    'block_names' => ['Menu'],
+                                    'title' => 'မာတိကာ'
+                                ]]
+                            ], [
+                                'title' => 'ပိုးတွေ့သေဆုံး လူနာ ' .  $this['deaths'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/ctyNhzt/six.png',
+                                'subtitle' => $this['dt'],
+                                'buttons' => [[
+
+                                    'type' => 'show_block',
+                                    'block_names' => ['Menu'],
+                                    'title' => 'မာတိကာ'
+                                ]]
+                            ],
+                            [
+                                'title' => 'ပိုးတွေ့ပြန်လည်သက်သာ လူနာ ' .  $this['recovered'] . ' ဦး',
+                                'image_url' => 'https://i.ibb.co/6sBJwfp/seven.png',
                                 'subtitle' => $this['dt'],
                                 'buttons' => [[
 
